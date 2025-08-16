977cc2f092aea66d03d37cd6f268c739d0b9a4b7
Josue Veliz <josuedemetriovelizescobar@gmail.com>
2025-08-16 08:16:58 -0400

    init project

---
+++ src/main/java/com/efms/employee_file_ms_be/EmployeeFileManagementSystemBeApplication.java
@@ -0,0 +1,13 @@
+package com.efms.employee_file_ms_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeFileManagementSystemBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeFileManagementSystemBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/efms/employee_file_ms_be/EmployeeFileManagementSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efms.employee_file_ms_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeFileManagementSystemBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
